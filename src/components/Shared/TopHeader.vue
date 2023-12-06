536700784ac3c48436f691b2886cee8068d46929
ToonSpanenburg <r0766397@student.thomasmore.be>
2023-12-06 02:10:01 +0100

    Header Fix

--- src/components/Shared/TopHeader.vue
+++ src/components/Shared/TopHeader.vue
@@ -5,8 +5,10 @@
     </h1>
       <a href="#/" class="p-1 link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Home</a>
       <a href="#/TheWheel" class="p-1 link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">The Wheel</a>
+    <!--
       <a href="#/SecretSatan" class="p-1 link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">SecretSatan</a>
       <a href="#/Test" class="p-1 link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">TestPage</a>
+    -->
   </div>
 
 </template>
