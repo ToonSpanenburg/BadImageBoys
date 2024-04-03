0c5845f298a7b50a20b1df155480f78213978a6b
ToonSpanenburg <r0766397@student.thomasmore.be>
2024-04-03 20:18:20 +0200

    first push

--- src/components/Article/ArticlePage.vue
+++ src/components/Article/ArticlePage.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="text-white">
     <h1>
-      {{myArticle.id}}
+      {{myArticle.title}}
     </h1>
+    <div>
+      <writer-component :writer="myArticle.writer"/>
+    </div>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur, debitis distinctio enim eveniet, explicabo labore, maiores nostrum quam quo sunt tempora vero voluptatem. Dicta distinctio optio quasi sunt tempore.
     </p>
@@ -13,6 +16,7 @@
 import ArticleComponent from "@/components/Home/ArticleComponent.vue";
 import {onMounted, ref} from "vue";
 import * as api from "@/components/api";
+import WriterComponent from "@/components/Article/WriterComponent.vue";
 
 let routeId = ref();
 const myArticle = ref({});
@@ -22,12 +26,14 @@ export default {
     next();
   },
 
-  components: {ArticleComponent},
+  components: {WriterComponent, ArticleComponent},
   setup() {
     onMounted(async () => {
       try {
         const article = await api.getData("article/" + routeId);
         myArticle.value = article;
+        console.log(myArticle.value);
+        console.log(myArticle.value.writer.username);
       } catch (error) {
         console.error('Error fetching articles:', error);
       }
---
+++ src/components/Article/WriterComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="writer">
+    <p>
+      {{writer.username}}
+    </p>
+  </div>
+</template>
+
+<script >
+export default {
+  props: {
+    writer: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
