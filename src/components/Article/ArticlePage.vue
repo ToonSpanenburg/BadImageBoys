937d41f004998eb77d90d25ef40c65043d408b8d
ToonSpanenburg <r0766397@student.thomasmore.be>
2024-04-03 18:39:01 +0200

    api fix

--- src/components/Article/ArticlePage.vue
+++ src/components/Article/ArticlePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-white">
     <h1>
-      {{$route.params.id}}
+      {{myArticle.id}}
     </h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur, debitis distinctio enim eveniet, explicabo labore, maiores nostrum quam quo sunt tempora vero voluptatem. Dicta distinctio optio quasi sunt tempore.
@@ -10,7 +10,33 @@
 </template>
 
 <script>
+import ArticleComponent from "@/components/Home/ArticleComponent.vue";
+import {onMounted, ref} from "vue";
+import * as api from "@/components/api";
 
+let routeId = ref();
+const myArticle = ref({});
+export default {
+  beforeRouteEnter(to, from, next) {
+    routeId = to.params.id;
+    next();
+  },
+
+  components: {ArticleComponent},
+  setup() {
+    onMounted(async () => {
+      try {
+        const article = await api.getData("article/" + routeId);
+        myArticle.value = article;
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    });
+    return {
+      myArticle
+    };
+  }
+};
 </script>
 
 <style scoped>
